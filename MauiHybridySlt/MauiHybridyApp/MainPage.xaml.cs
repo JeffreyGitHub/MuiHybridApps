182c301e64eabef93931a82704273e029b87747d
Jianfeng Chen <jianfeng.chen@statcan.gc.ca>
2024-11-28 09:29:49 -0500

    Created a default MauiHybrid app, added a default Maui controller by using the grid in Mainpage class.

---
+++ MauiHybridySlt/MauiHybridyApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiHybridyApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        // string cultureName = 
+        //     CultureInfo.CurrentCulture.Name=="en-US"?"fr-FR":"en-US";
+		      //
+        // var aUpdateCultureAction = new UpdateCultureAction(UpdateCulture)
+        // {
+        //     IsCultureChanged = true, 
+        //     Culture = cultureName
+        // };
+        // await blazorWebView.TryDispatchAsync(serviceProvider =>
+        // {
+        //     Dispatcher = serviceProvider.GetRequiredService<Fluxor.IDispatcher>();
+			     //
+        // });
+		      //
+        // Dispatcher.Dispatch(aUpdateCultureAction); //  Culture changes in Fluxor components
+    }
+}
